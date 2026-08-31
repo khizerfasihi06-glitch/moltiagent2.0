897386ee39bd8d04cf006d1072472f70fa4fcf01
Khizar Fasihi <khizerfasihi06@gmail.com>
2026-08-31 15:29:34 +0500

    Update UI.py

--- UI.py
+++ UI.py
@@ -8,7 +8,7 @@ from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from mistralai import Mistral
+from mistralai.client import MistralClient
 
 
 st.set_page_config(page_title=" Avatar AI Experts", page_icon="💧", layout="centered")
